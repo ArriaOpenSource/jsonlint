2be34aacb3a556ee4be17d1ef96d4ba590e7632f
Renaldas <renaldas.kerpe@arria.com>
2019-01-30 16:34:29 +0000

    Create a basic definitions file for use within TypeScript.

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare namespace jsonlint {
+  var formatter: any;
+  var parser: any;
+  var parse: any;
+  export {formatter, parser, parse};
+}
+export = jsonlint;
---
+++ index.js
@@ -0,0 +1,5 @@
+var formatter = require('./lib/formatter').formatter;
+var parser = require('./lib/jsonlint').parser;
+var parse = require('./lib/jsonlint').parse;
+
+module.exports = {formatter, parser, parse};
